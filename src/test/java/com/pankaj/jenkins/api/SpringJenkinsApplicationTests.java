050b99c70c85ff422c79e8e9a01e21efcbc60d41
pankajgu <pankajgupta6362@gmail.com>
2025-02-23 18:34:28 +0530

    removed assert equal

--- src/test/java/com/pankaj/jenkins/api/SpringJenkinsApplicationTests.java
+++ src/test/java/com/pankaj/jenkins/api/SpringJenkinsApplicationTests.java
@@ -1,15 +1,15 @@
 package com.pankaj.jenkins.api;
 
-import static org.junit.Assert.assertEquals;
+
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
+
+
+
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 	
@@ -18,8 +18,6 @@ class SpringJenkinsApplicationTests {
 	@Test
 	void contextLoads() {
 		logger.info("Test case Executing...");
-		assertEquals(true, true);
-		assertEquals(false, false);
 	}
 
 }
